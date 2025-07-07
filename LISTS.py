3df5180c07448ad1b9c2306ecdf6bdcfc59c5add
ReBullet <admin@rebullet.ch>
2025-07-07 09:57:39 +0200

    UPDATE

--- LISTS.py
+++ LISTS.py
@@ -1,7 +1,7 @@
 import requests
 
 urls = [
-    "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts",
+    "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/ultimate.txt"
 ]
 
